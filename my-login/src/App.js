4789318b400ac4197a5eec701c7414e35aaad625
Kanghee <kh63167574@gmail.com>
2025-05-22 22:01:34 +0900

    setting2

--- my-login/src/App.js
+++ my-login/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [showPsw,setShowPsw] = useState(false);
+
+  const toggleShowPsw = () =>{
+    setShowPsw(!showPsw);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div style={title}>회원가입구현 과제</div>
+    <div style ={divStyle}>
+        
+      <input style = {inputStyle} type="email" placeholder='이메일입력'></input>
+      <div>
+      <input class = "psw" style = {inputStyle} type={showPsw ? "password":"text"} placeholder='비밀번호입력'></input>
+      <button onClick={toggleShowPsw}> {(!showPsw ? "🙈" : "👁")}</button>
+      </div>
+      <input style = {inputStyle} type="password" placeholder='비밀번호 재입력'></input>
+      <input style = {inputStyle} type="text" placeholder='이름'></input>
+      <input style = {inputStyle} type="text" placeholder='생년월일'></input>
+      <button style={buttonStyle} >회원가입!</button> 
+
     </div>
+    </div>
+    
   );
+}
+
+const title = {
+  display : "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "40px",
+}
+
+const divStyle = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "12px",
+  backgroundColor : "skyblue",
+  height : "100vh",
+}
+
+const inputStyle = {
+  width : "250px",
+  height : "30px",
+  borderRadius : "30px",
+
+}
+
+const buttonStyle ={
+  borderRadius: "30px",
+  width : "150px",
+  height : "30px",
 }
 
 export default App;
